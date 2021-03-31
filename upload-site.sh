4c05d70e1f966fcfd95aef029aff990c0f5fdfce
Kyle Harrington <mdc@kyleharrington.com>
2021-03-31 07:54:44 +0200

    Add new username to script

--- upload-site.sh
+++ upload-site.sh
@@ -31,7 +31,7 @@ esac
 echo "Found launcher: $launcher"
 
 # upload complete update site
-password=$WIKI_UPLOAD_PASS
+password=$UPDATE_SITE_PASSWORD
 ./$launcher --update edit-update-site $update_site $url "webdav:$webdav_user:$password" .
 ./$launcher --update upload-complete-site --force --force-shadow $update_site
 
