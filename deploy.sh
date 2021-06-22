979bfdb127d44b25ecfe6f10656f16ee0d197c53
Kyle Harrington <mdc@kyleharrington.com>
2021-06-22 14:00:16 +0200

    Update output for correct update site name

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ echo "====== Generating Fiji installation ======"
 sh populate_fiji.sh
 
 echo
-echo "====== Uploading to sciview-ageratum update site ======"
+echo "====== Uploading to sciview-buttercup update site ======"
 cd Fiji.app
 chmod a+x ../upload-site.sh
 ../upload-site.sh sciview-buttercup sciview-update-sites-ci
